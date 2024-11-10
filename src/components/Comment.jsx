aa9796870de81cdaf766f445b9ccde292e79ee25
Kawan02 <kawanng39@gmail.com>
2024-11-10 15:45:18 -0300

    wip: seção de comentário em andamento

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/kawan02.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Kawan Messias</strong>
+                            <time title='10 de Novembro às 13:41h' dateTime="2024-11-10 13:41:00">Cerca há 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Messias, parabéns!!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
