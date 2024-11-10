d4a2f929ceb30d1e8e7d27b808372c5e613d51c8
Kawan02 <kawanng39@gmail.com>
2024-11-10 14:11:20 -0300

    feat: seção de post em andamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
-import './global.css'
-
-import styles from './App.module.css'
 import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
+
+import './global.css'
+import styles from './App.module.css'
+
 
 export function App() {
 
@@ -14,6 +15,7 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Post />
           <Post />
         </main>
       </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,33 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/kawan02.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Kawan Messias</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='10 de Novembro às 13:41h' dateTime="2024-11-10 13:41:00">Publica há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+}
